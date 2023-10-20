f86c0be35f6538d9a2da7a75774085cdd95e116b
David Campo Casas <david.campo.casas@gmail.com>
2023-10-20 22:47:57 +0200

    Update userCarController.js

--- controllers/userCarController.js
+++ controllers/userCarController.js
@@ -1,79 +1,32 @@
-const UserCar = require('../models/userCarModel'); // Asume que tienes un modelo de relación de usuario con coches
+const UserCar = require('../models/userCarModel');
+const { connectToDatabase } = require('../database/database');
 
-/**
- * @swagger
- * /userCars/addFavoriteCar:
- *   post:
- *     summary: Agregar un coche a la lista de favoritos de un usuario.
- *     tags: [Usuarios y Coches]
- *     description: Agrega un coche a la lista de favoritos de un usuario.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               userId:
- *                 type: string
- *               carId:
- *                 type: string
- *             example:
- *               userId: "usuario123"
- *               carId: "coche456"
- *     responses:
- *       '200':
- *         description: Coche agregado a favoritos con éxito.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: Coche agregado a favoritos con éxito
- *       '400':
- *         description: El coche ya está en la lista de favoritos del usuario.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   example: El coche ya está en la lista de favoritos del usuario
- *       '500':
- *         description: Error interno del servidor.
- */
-const addFavoriteCar = async (req, res) => {
+const toggleFavoriteCar = async (req, res) => {
   try {
     const { userId, carId } = req.body;
-
-    // Verifica si el usuario ya tiene el coche en su lista de favoritos
-    const existingFavorite = await UserCar.findOne({ userId, carId });
-
+    const db = await connectToDatabase();
+    
+    const existingFavorite = await db.collection('userCars').findOne({ userId, carId });
+    
     if (existingFavorite) {
-      return res.status(400).json({ error: 'El coche ya está en la lista de favoritos del usuario' });
+      // Si el coche ya está en favoritos, lo eliminamos
+      await db.collection('userCars').deleteOne({ userId, carId });
+      res.status(200).json({ message: 'Coche eliminado de favoritos con éxito' });
+    } else {
+      // Si el coche no está en favoritos, lo agregamos
+      await db.collection('userCars').insertOne({
+        userId,
+        carId,
+      });
+    
+      res.status(200).json({ message: 'Coche agregado a favoritos con éxito' });
     }
-
-    // Crea una nueva relación de usuario con coche en la base de datos
-    const newUserCar = new UserCar({
-      userId,
-      carId,
-      // Otras propiedades, como fecha de adición, calificaciones, comentarios, etc.
-    });
-
-    await newUserCar.save();
-
-    res.json({ message: 'Coche agregado a favoritos con éxito' });
   } catch (error) {
-    res.status(500).json({ error: 'Error al agregar el coche a favoritos' });
+    console.error(error);
+    res.status(500).json({ error: 'Error interno del servidor al gestionar el coche en favoritos' });
   }
 };
 
-// Otros controladores relacionados con relaciones de usuario con coches (eliminar favorito, obtener favoritos, etc.)
-
 module.exports = {
-  addFavoriteCar,
-  // Otros controladores
+  toggleFavoriteCar,
 };
