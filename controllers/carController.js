23712f44f077b8741a8605e0f1b302e405038488
David Campo Casas <david.campo.casas@gmail.com>
2023-10-22 12:09:03 +0200

    some cleaning and refactors

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,4 +1,4 @@
-const { connectToDatabase } = require('../database/database');
+const { connectToDatabase, closeDatabaseConnection } = require('../database/database');
 
 /**
  * @swagger
@@ -27,46 +27,29 @@ const getCars = async (req, res) => {
     const userName = req.query.userName;
     
     if (userName) {
-      // Si se proporciona el nombre de usuario, busca los modelos de coches marcados como favoritos por ese usuario
       const userFavoriteModels = await db.collection('userCars').find({ userName }).toArray();
       
       carsData.forEach((brand) => {
-        console.log(`Marca: ${brand.nombre}`); // Agregar una traza de la marca
         if (brand.marcas && Array.isArray(brand.marcas)) {
           brand.marcas.forEach((marca) => {
             if (marca.modelos && Array.isArray(marca.modelos)) {
               marca.modelos.forEach((modelo) => {
                 if (modelo && modelo._id) {
-                  console.log(`Modelo ID: ${modelo._id}`); // Agregar una traza del ID del modelo
                   const modelId = modelo._id;
-                  // Verificar si el modelo existe en userFavoriteModels
                   const favoriteModel = userFavoriteModels.find((favoriteModel) => favoriteModel.carId === modelId);
-                  if (favoriteModel) {
-                    console.log(`El modelo ${modelId} es favorito.`);
-                  } else {
-                    console.log(`El modelo ${modelId} no es favorito.`);
-                  }
                   modelo.isFavorite = !!favoriteModel;
-                } else {
-                  console.error('Datos de modelo incorrectos:', modelo);
                 }
               });
-            } else {
-              console.error('Datos de modelos incorrectos en la marca:', marca);
             }
           });
-        } else {
-          console.error('Datos de marcas incorrectos en la marca:', brand);
         }
       });
     }
     
     res.json(carsData);
   } catch (error) {
-    console.error('Error al obtener la lista de coches:', error);
     res.status(500).json({ error: 'Error al obtener la lista de coches' });
   } finally {
-    // Cierra la conexión a la base de datos de manera segura.
     closeDatabaseConnection();
   }
 };
--- controllers/userCarController.js
+++ controllers/userCarController.js
@@ -49,23 +49,18 @@ const addFavoriteCar = async (req, res) => {
     const existingFavorite = await db.collection('userCars').findOne({ userName, carId });
     
     if (existingFavorite) {
-      // Si el coche ya está en favoritos, lo eliminamos
       await db.collection('userCars').deleteOne({ userName, carId });
       res.status(200).json({ message: 'Coche eliminado de favoritos con éxito' });
     } else {
-      // Si el coche no está en favoritos, lo agregamos
       await db.collection('userCars').insertOne({
         userName,
         carId,
       });
-    
       res.status(200).json({ message: 'Coche agregado a favoritos con éxito' });
     }
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: 'Error interno del servidor al gestionar el coche en favoritos' });
   } finally {
-    // Cierra la conexión a la base de datos de manera segura.
     closeDatabaseConnection();
   }
 };
@@ -101,19 +96,13 @@ const addFavoriteCar = async (req, res) => {
  *                   type: string
  */
 const getDataDashboard = async (req, res) => {
-  let db;
   try {
-    // Conecta a la base de datos.
-    db = await connectToDatabase();
-
-    // Consulta la base de datos para obtener los datos de usuarios y sus coches favoritos.
+    const db = await connectToDatabase();
     const userCarsData = await db.collection('userCars').find().toArray();
     const carsData = await db.collection('cars').find().toArray();
 
-    // Crear un objeto para almacenar la información de coches por usuario.
     const cochesPorUsuario = {};
 
-    // Procesar los datos para contar coches distintos por usuario y obtener la información de marca y modelo.
     for (const userCar of userCarsData) {
       const userName = userCar.userName;
       const carId = userCar.carId;
@@ -122,15 +111,11 @@ const getDataDashboard = async (req, res) => {
         cochesPorUsuario[userName] = { coches: [] };
       }
 
-      // Consulta la información de marca y modelo en la colección "cars" utilizando el carId anidado.
-      const carInfo = carsData.find(car => {
-        return car.marcas.some(marca =>
-          marca.modelos.some(modelo => modelo._id === carId)
-        );
-      });
+      const carInfo = carsData.find(car => car.marcas.some(marca =>
+        marca.modelos.some(modelo => modelo._id === carId)
+      ));
 
       if (carInfo) {
-        // Encuentra la marca y modelo correspondientes dentro de la estructura anidada.
         const marca = carInfo.marcas.find(marca =>
           marca.modelos.some(modelo => modelo._id === carId)
         );
@@ -143,16 +128,13 @@ const getDataDashboard = async (req, res) => {
       }
     }
 
-    // Agregar el número de coches distintos en la respuesta.
     Object.keys(cochesPorUsuario).forEach(userName => {
       cochesPorUsuario[userName].numCoches = cochesPorUsuario[userName].coches.length;
     });
 
-    // Crear objetos para el ranking de marcas y modelos.
     const rankingMarcas = {};
     const rankingModelos = {};
 
-    // Procesar los datos para el ranking de marcas y modelos.
     for (const userName in cochesPorUsuario) {
       for (const coche of cochesPorUsuario[userName].coches) {
         const marca = coche.marca;
@@ -172,28 +154,22 @@ const getDataDashboard = async (req, res) => {
       }
     }
 
-    // Ordenar los rankings de marcas y modelos de mayor a menor.
     const sortedRankingMarcas = Object.entries(rankingMarcas).sort((a, b) => b[1] - a[1]);
     const sortedRankingModelos = Object.entries(rankingModelos).sort((a, b) => b[1] - a[1]);
 
-    // Crear un objeto de respuesta con la información de coches por usuario y los rankings ordenados.
     const response = {
       cochesPorUsuario,
       rankingMarcas: Object.fromEntries(sortedRankingMarcas),
       rankingModelos: Object.fromEntries(sortedRankingModelos),
     };
 
-    // Enviar respuesta
     res.status(200).json(response);
   } catch (error) {
-    console.error('Error al obtener información del dashboard:', error);
     res.status(500).json({ error: 'Error al obtener información del dashboard' });
   } finally {
-    // Cierra la conexión a la base de datos de manera segura.
     closeDatabaseConnection();
   }
 };
-
 
 module.exports = {
   addFavoriteCar,
